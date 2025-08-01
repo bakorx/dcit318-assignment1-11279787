37d716094d289a02975905bc8f89633050aa5696
bakorx <asnkwamebakor@gmail.com>
2025-08-01 19:46:54 +0000

    ticket function

---
+++ TicketPriceCalculator/Ticket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class TicketCalculator
+{
+    static int TicketPrice(int age)
+    {
+        if (age <= 12 || age >= 65)
+            return 7;
+        else
+            return 10;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        string input = Console.ReadLine();
+
+        int age;
+        if (int.TryParse(input, out age) && age >= 0)
+        {
+            int price = TicketPrice(age);
+            Console.WriteLine($"Your ticket price is GHC{price}.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid age entered.");
+        }
+
+        Console.WriteLine("\nPress Enter to exit...");
+        Console.ReadLine();
+    }
+}
